61c4818ad1d95ebbc923d5983e461e69deb038db
Andrei-Silviu DRAGNEA (24887) <andrei.dragnea@stud.acs.upb.ro>
2017-07-10 17:08:44 +0300

    Added inspection tests.

---
+++ RemoveRecursionRefactoring/tests/ro/pub/cs/diploma/RemoveRecursionInspectionTest.java
@@ -0,0 +1,29 @@
+package ro.pub.cs.diploma;
+
+import com.intellij.codeInspection.InspectionProfileEntry;
+import com.siyeh.ig.LightInspectionTestCase;
+import org.jetbrains.annotations.Nullable;
+
+/*
+ * @see com.siyeh.ig.performance.TailRecursionInspectionTest
+ */
+public class RemoveRecursionInspectionTest extends LightInspectionTestCase {
+  @Nullable
+  @Override
+  protected InspectionProfileEntry getInspection() {
+    return new RemoveRecursionInspection();
+  }
+
+  @Override
+  protected String getTestDataPath() {
+    return "testdata";
+  }
+
+  public void testLambdaWithReturnStmt() {
+    doTest();
+  }
+
+  public void testTailRecursion() {
+    doTest();
+  }
+}
