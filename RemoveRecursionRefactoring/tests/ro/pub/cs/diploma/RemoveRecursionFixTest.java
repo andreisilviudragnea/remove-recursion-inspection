ed9062f1865a35080f4c058d9867dbc2b80f9954
Andrei-Silviu DRAGNEA (24887) <andrei.dragnea@stud.acs.upb.ro>
2017-07-12 14:05:16 +0300

    Made some changes.

--- RemoveRecursionRefactoring/tests/ro/pub/cs/diploma/RemoveRecursionFixTest.java
+++ RemoveRecursionRefactoring/tests/ro/pub/cs/diploma/RemoveRecursionFixTest.java
@@ -4,7 +4,6 @@ package ro.pub.cs.diploma;
  * @see com.siyeh.ig.fixes.performance.RemoveTailRecursionFixTest
  */
 public class RemoveRecursionFixTest extends IGQuickFixesTestCase {
-
   @Override
   protected void setUp() throws Exception {
     super.setUp();
@@ -18,12 +17,59 @@ public class RemoveRecursionFixTest extends IGQuickFixesTestCase {
     return "testdata";
   }
 
-  public void testCallOnOtherInstance1() { doTest(); }
-  public void testCallOnOtherInstance2() { doTest(); }
-  public void testDependency1() { doTest(); }
-  public void testDependency2() { doTest(); }
-  public void testDependency3() { doTest(); }
-  public void testDependency4() { doTest(); }
-  public void testThisVariable() { doTest(); }
-  public void testUnmodifiedParameter() { doTest(); }
+  public void testCallOnOtherInstance1() {
+    doTest();
+  }
+
+  public void testCallOnOtherInstance2() {
+    doTest();
+  }
+
+  public void testDependency1() {
+    doTest();
+  }
+
+  public void testDependency2() {
+    doTest();
+  }
+
+  public void testDependency3() {
+    doTest();
+  }
+
+  public void testDependency4() {
+    doTest();
+  }
+
+  public void testThisVariable() {
+    doTest();
+  }
+
+  public void testUnmodifiedParameter() {
+    doTest();
+  }
+
+  public void testArrayPrinter1() {
+    doTest();
+  }
+
+  public void testArrayPrinter2() {
+    doTest();
+  }
+
+  public void testFib1() {
+    doTest();
+  }
+
+  public void testFib2() {
+    doTest();
+  }
+
+  public void testFactorial1() {
+    doTest();
+  }
+
+  public void testFactorial2() {
+    doTest();
+  }
 }
--- RemoveRecursionRefactoring/tests/ro/pub/cs/diploma/RemoveRecursionInspectionTest.java
+++ RemoveRecursionRefactoring/tests/ro/pub/cs/diploma/RemoveRecursionInspectionTest.java
@@ -1,13 +1,21 @@
 package ro.pub.cs.diploma;
 
 import com.intellij.codeInspection.InspectionProfileEntry;
+import com.intellij.testFramework.LightProjectDescriptor;
 import com.siyeh.ig.LightInspectionTestCase;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /*
  * @see com.siyeh.ig.performance.TailRecursionInspectionTest
  */
 public class RemoveRecursionInspectionTest extends LightInspectionTestCase {
+  @NotNull
+  @Override
+  protected LightProjectDescriptor getProjectDescriptor() {
+    return new DefaultLightProjectDescriptor();
+  }
+
   @Nullable
   @Override
   protected InspectionProfileEntry getInspection() {
