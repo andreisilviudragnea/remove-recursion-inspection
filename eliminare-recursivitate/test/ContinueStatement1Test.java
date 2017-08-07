d9f3882b87203112530a036eae55c14f5befdd02
Andrei-Silviu DRAGNEA (24887) <andrei.dragnea@stud.acs.upb.ro>
2017-08-07 19:36:47 +0300

    Added new continue statement test.

---
+++ eliminare-recursivitate/test/ContinueStatement1Test.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ContinueStatement1Test {
+  @Test
+  public void cont() throws Exception {
+    final List<Integer> list = new ArrayList<>();
+    ContinueStatement1.cont(4, list);
+    assertEquals(Arrays.asList(4, 3, 2, 1, 1, 2, 1, 1, 2, 3, 2, 1, 1, 2, 1, 1, 2, 3, 2, 1, 1, 2, 1, 1, 2, 3, 4, 3, 2, 1, 1, 2, 1, 1, 2, 3, 2, 1, 1, 2, 1, 1, 2, 3, 2, 1, 1, 2, 1, 1, 2, 3, 4, 3, 2, 1, 1, 2, 1, 1, 2, 3, 2, 1, 1, 2, 1, 1, 2, 3, 2, 1, 1, 2, 1, 1, 2, 3, 4), list);
+  }
+
+}
