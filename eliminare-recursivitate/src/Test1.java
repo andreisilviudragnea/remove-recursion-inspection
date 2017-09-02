2a2fc33f251de654bfafa1a5831d4a62365edf5c
Andrei-Silviu DRAGNEA (24887) <andrei.dragnea@stud.acs.upb.ro>
2017-09-02 21:47:54 +0300

    Added two snippets.

---
+++ eliminare-recursivitate/src/Test1.java
@@ -0,0 +1,7 @@
+public class Test1 {
+  public static void main(String[] args) {
+//    int i;
+    for (int i = 0; i < 10; i++)
+      System.out.println(i);
+  }
+}
---
+++ eliminare-recursivitate/src/Test3.java
@@ -0,0 +1,9 @@
+class Test3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            System.out.print(i + " ");
+        for (int i = 10; i > 0; i--)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+}
